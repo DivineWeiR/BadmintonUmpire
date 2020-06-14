38592480a4a1e6c51c059c6ef1122bf8cd6a2287
WeiR <wr188@foxmail.com>
2020-06-14 17:17:57 +0800

    Add rotate and score plus function for score button

--- assets/js/umpire.js
+++ assets/js/umpire.js
@@ -1,7 +1,31 @@
 function scorePlus(scoreEnd) {
-    var scoreBody = $("#" + scoreEnd + "-score");
-    scoreBody.text(parseInt(scoreBody.text())+1);
-    // scoreBody.innerHTML = parseInt(scoreBody.innerHTML) + 1;
+    let anotherEnd = "";
+    if (scoreEnd == "left") {
+        anotherEnd = "right";
+    } else if (scoreEnd == "right") {
+        anotherEnd = "left";
+    }
+    const scoreBody = $("#" + scoreEnd + "-score");
+    const scoreAnother = $("#" + anotherEnd + "-score");
+    const serveIcon = $("#serve-icon");
+
+    //Set score
+    const currentScore = parseInt(scoreBody.text())+1;
+    scoreBody.text(currentScore);
+
+    //Set rotate style of scoreboard
+    if (scoreBody.css("transform") == "matrix3d(1, 0, 0, 0, 0, 1, -2.44929e-16, 0, 0, 2.44929e-16, 1, 0, 0, 0, 0, 1)"||
+    scoreBody.css("transform")=="rotateX(360deg)") {
+        scoreBody.css("transform","");
+    } else {
+        scoreBody.css("transform","rotateX(360deg)");
+    }
+
+    //Set serve color of scoreboard
+    scoreBody.addClass("serve-score");
+    scoreAnother.removeClass("serve-score");
+
+
 }
 
 function rotate(deg) {
